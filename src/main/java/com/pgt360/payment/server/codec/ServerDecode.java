13299cedb7f4307b01494e2e0cb86387678582e5
Guiller <learsi.guil2010@gmail.com>
2022-02-04 13:51:36 -0400

    string decoder

--- src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
+++ src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
@@ -6,6 +6,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+
+
 import java.util.List;
 
 public class ServerDecode extends ByteToMessageDecoder {
--- src/main/java/com/pgt360/payment/server/config/ServerInitializer.java
+++ src/main/java/com/pgt360/payment/server/config/ServerInitializer.java
@@ -9,19 +9,22 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
-    private final ServerDecode serverDecode = new ServerDecode();
+    //private final ServerDecode serverDecode = new ServerDecode();
     private final ServerHandler serverHandler = new ServerHandler();
+    private final StringDecoder stringDecoder = new StringDecoder();
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
         pipeline.addLast(new DelimiterBasedFrameDecoder(1024 * 1024, Delimiters.lineDelimiter()));
-        pipeline.addLast(serverDecode);
+        pipeline.addLast(stringDecoder);
         pipeline.addLast(serverHandler);
     }
 }
