2c33111e0d6ddb0902697de90a1ab5cc9985d65b
Guiller <learsi.guil2010@gmail.com>
2022-02-04 12:55:04 -0400

    Update decode

--- src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
+++ src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
@@ -16,13 +16,20 @@ public class ServerDecode extends ByteArrayDecoder {
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
         System.out.println("Mensaje de entrada para de codificar:"+in.readInt());
         if(in.readableBytes()>0){
-            byte[] data = new byte[in.readableBytes()];
+            /*byte[] data = new byte[in.readableBytes()];
             for(int i = 0; i < in.readableBytes(); i++){
                 data[i] = in.getByte(i);
             }
             //ByteBuffer buffer = ByteBuffer.wrap(data);
             String hex = NettyUtil.bytesToHex(data);
-            out.add(hex);
+            out.add(hex);*/
+            // copia el contenido de ByteBuf a una matriz de bytes
+            // copy the ByteBuf content to a byte array
+            byte[] array = new byte[in.readableBytes()];
+            in.getBytes(0, array);
+            String hex = NettyUtil.bytesToHex(array);
+            out.add(array);
+
             System.out.println("Mensaje decodificado y enviado correctamente");
         }else{
             System.out.println("************No existe bytes legibles!**********");
