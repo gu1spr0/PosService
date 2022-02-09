8219c9df2a0c16db2d0db6d4990aa406a7b641c6
Guiller <learsi.guil2010@gmail.com>
2022-02-09 17:38:01 -0400

    Tests bytes

--- src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
+++ src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
@@ -5,11 +5,21 @@ import com.pgt360.payment.util.NettyUtil;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.util.CharsetUtil;
+
 import java.util.List;
 
 public class ServerDecode extends ByteToMessageDecoder {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if(in.readableBytes()>0){
+            String codRespuesta = in.toString(CharsetUtil.UTF_8);
+            System.out.println("Tamaño:"+in.readableBytes());
+            String data = codRespuesta.substring(50,4);
+            System.out.println("Respuesta:"+NettyUtil.hex2a(data));
+
+
+        }
         /*if(in.readableBytes()>0){
             String msg = in.toString(CharsetUtil.UTF_8);
             in.readerIndex(in.readerIndex()+in.readableBytes());
