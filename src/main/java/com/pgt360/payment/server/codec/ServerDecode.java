984a331437b9fc7519e1fe0666e465c1a4ad30d3
Guiller <learsi.guil2010@gmail.com>
2022-02-10 19:04:08 -0400

    Mensaje

--- src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
+++ src/main/java/com/pgt360/payment/server/codec/ServerDecode.java
@@ -10,17 +10,13 @@ import io.netty.util.CharsetUtil;
 import java.util.List;
 
 public class ServerDecode extends ByteToMessageDecoder {
-    int i = 0;
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        String response = "";
         if(in.readableBytes()>0){
             String respuesta = in.toString(CharsetUtil.UTF_8);
             String hex = NettyUtil.bytesToHex(respuesta.getBytes());
-            response = response + hex;
-            System.out.println(response);
-            out.clear();
             out.add(hex);
+            System.out.println("Mensaje enviado al handler:"+hex);
         }
     }
 }
