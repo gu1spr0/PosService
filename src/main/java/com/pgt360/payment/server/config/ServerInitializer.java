c0a3012706f2b12484f4512aa396b41734dd14e6
Israel Guillermo <learsi.guil2010@gmail.com>
2022-06-17 14:14:57 -0400

    Cambios

--- src/main/java/com/pgt360/payment/server/config/ServerInitializer.java
+++ src/main/java/com/pgt360/payment/server/config/ServerInitializer.java
@@ -4,7 +4,6 @@ import com.pgt360.payment.server.codec.ServerDecode;
 import com.pgt360.payment.server.handler.ServerHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.timeout.ReadTimeoutHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel){
-        socketChannel.pipeline().addLast(new ServerDecode(), new ReadTimeoutHandler(60));
+        socketChannel.pipeline().addLast(new ServerDecode());
         socketChannel.pipeline().addLast(new ServerHandler());
     }
 }
